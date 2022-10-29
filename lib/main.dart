1774247312574f98ceb5df9d53aa618381002118
chosun <codefordmr01@naver.com>
2022-10-29 10:25:56 +0900

    Checkbox, Switch

--- lib/main.dart
+++ lib/main.dart
@@ -30,38 +30,32 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController controller = TextEditingController();
+  var val = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("첫번째 플루터 프로젝트"),
-        ),
-        body: Column(
-          children: [
-            Container(
-              width: 100,
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: "아이디를 입력해주세요",
-                  labelText: "아이디",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-                controller: controller,
-                onChanged: (text) {
-                  print(text);
-                }
-    ),
-            ), TextButton(
-              child: Text('회원가입'),
-              onPressed: () {
-                print(controller.text);
-              },
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text("첫번째 플루터 프로젝트"),
+      ),
+      body: Column(
+        children: [
+          Checkbox(
+              value: val,
+              onChanged: (value) {
+                setState(() {
+                  val = value!;
+                });
+              }),
+          Switch(
+              value: val,
+              onChanged: (value) {
+                setState(() {
+                  val = value!;
+                });
+              }),
+        ],
+      ),
     );
   }
 }
